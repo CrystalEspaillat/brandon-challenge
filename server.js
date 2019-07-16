1c3b78b44d1d52f202705466c5f33f5d1bc84a59
Crystal Espaillat <crystalespaillat@Crystals-MacBook-Pro.local>
2019-07-15 22:35:34 -0400

    started site

---
+++ server.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+const express = require('express');
+const path = require('path');
+// const bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
+
+// EXPRESS
+const app = express();
+
+// SERVE STATIC FILES
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// // BODY PARSER
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// DISPLAY PAGE
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/pages/index.html'));
+});
+
+// LISTEN
+app.listen(process.env.PORT || 5000, () => console.log('Server started...'));
